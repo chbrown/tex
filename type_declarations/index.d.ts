cbe488493e92ec6a3297a7a8abb7852e9a4ab79a
Christopher Brown <io@henrian.com>
2015-07-22 12:49:53 -0500

    Bump type declarations with latest from DT upstream and merge DT imports into index.d.ts

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,4 +1,9 @@
-/// <reference path="DefinitelyTyped.d.ts" />
+/// <reference path="DefinitelyTyped/node/node.d.ts" />
+/// <reference path="DefinitelyTyped/async/async.d.ts" />
+/// <reference path="DefinitelyTyped/chalk/chalk.d.ts" />
+/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
+/// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
+/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
 
 // self-declaring packages:
 /// <reference path="../node_modules/lexing/lexing.d.ts" />
