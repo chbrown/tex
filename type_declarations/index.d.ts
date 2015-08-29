5f1fb94acfd8db921a519bd1cbdee852998763ad
Christopher Brown <io@henrian.com>
2015-08-28 20:06:47 -0500

    Remove unused type declarations; update others

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,24 +1,7 @@
 /// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/async/async.d.ts" />
-/// <reference path="DefinitelyTyped/chalk/chalk.d.ts" />
-/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
 /// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
 /// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
 
 // self-declaring packages:
 /// <reference path="../node_modules/lexing/lexing.d.ts" />
-
-// internal type declarations:
-declare module "loge" {
-  interface Logger {
-    level: string;
-    silly(...args: any[]): void;
-    debug(...args: any[]): void;
-    info(...args: any[]): void;
-    warn(...args: any[]): void;
-    error(...args: any[]): void;
-    critical(...args: any[]): void;
-  }
-  var logger: Logger;
-  export = logger;
-}
+/// <reference path="../node_modules/loge/loge.d.ts" />
