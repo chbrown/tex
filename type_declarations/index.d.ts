26d44dbc70b0f453257d90168c291aedf856feee
Christopher Brown <io@henrian.com>
2015-11-02 13:07:40 -0600

    Remove local type_declarations

--- type_declarations/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
-/// <reference path="DefinitelyTyped/yargs/yargs.d.ts" />
-
-// self-declaring packages:
-/// <reference path="../node_modules/lexing/lexing.d.ts" />
-/// <reference path="../node_modules/loge/loge.d.ts" />
