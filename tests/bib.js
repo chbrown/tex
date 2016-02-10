d738471e553fceb1f1000a24fa3aa833624e92d5
Christopher Brown <io@henrian.com>
2016-02-10 12:42:25 -0600

    Fix tests to match API change

--- tests/bib.js
+++ tests/bib.js
@@ -4,7 +4,7 @@ import {readdirSync, readFileSync} from 'fs';
 import {join} from 'path';
 import {nfc} from 'unorm';
 
-import {parseBibTeXEntry} from '../';
+import {parseBibTeXEntry, flattenBibTeXEntry} from '../';
 
 function normalizeObject(object) {
   for (var key in object) {
@@ -13,19 +13,22 @@ function normalizeObject(object) {
   return object;
 }
 
+function compareBibJson(bibPath, jsonPath = bibPath.replace(/bib$/, 'json')) {
+  it(`should parse ${bibPath} into ${jsonPath}`, () => {
+    const actual_data = readFileSync(bibPath, {encoding: 'utf8'});
+    const actualBibTeXEntry = parseBibTeXEntry(actual_data);
+    assert(actualBibTeXEntry, 'parse result is empty');
+    const actualReference = normalizeObject(flattenBibTeXEntry(actualBibTeXEntry));
+    const expectedReference = normalizeObject(JSON.parse(readFileSync(jsonPath, {encoding: 'utf8'})));
+    assert.deepEqual(actualReference, expectedReference);
+  });
+}
+
 describe('BibTeX parser', () => {
-  var dirpath = join(__dirname, 'bibfiles');
-  var filenames = readdirSync(dirpath).filter(file => file.match(/bib$/) != null);
-  filenames.forEach(filename => {
-    var bib_filepath = join(dirpath, filename);
-    var json_filepath = bib_filepath.replace(/bib$/, 'json');
-    it(`should parse ${bib_filepath} into ${json_filepath}`, () => {
-      var actual_data = readFileSync(bib_filepath, {encoding: 'utf8'});
-      var actual_bibtex_entry = parseBibTeXEntry(actual_data);
-      assert(actual_bibtex_entry, 'parse result is empty');
-      var actual = normalizeObject(actual_bibtex_entry.toJSON());
-      var expected = normalizeObject(JSON.parse(readFileSync(json_filepath, {encoding: 'utf8'})));
-      assert.deepEqual(actual, expected);
-    });
+  const dirpath = join(__dirname, 'bibfiles');
+  readdirSync(dirpath)
+  .filter(file => file.match(/bib$/) != null)
+  .forEach(filename => {
+    compareBibJson(join(dirpath, filename));
   });
 });
