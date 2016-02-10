d56e23e92c6772635e6b8c88324f135127301e72
Christopher Brown <io@henrian.com>
2016-02-10 12:37:09 -0600

    Remove compiled output from source control

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ParentNode } from './dom';
-import { BibTeXEntry } from './models';
-export declare function parseBibTeXEntry(string: string): BibTeXEntry;
-export declare function parseBibTeXEntries(string: string): BibTeXEntry[];
-export declare function parseNode(tex: string): ParentNode;
-export declare function extractCitekeys(tex: string): string[];
